5aaa4dfe1f96e5181404969baf0bd3a8d4e4b028
saiteja29 <44159659+saiteja29@users.noreply.github.com>
2019-08-03 16:30:19 +0530

    Create pr10.py

---
+++ pr10.py
@@ -0,0 +1,8 @@
+chaaa=int(input())
+choa=[int(o) for o in input().split(" ")]
+chea=0
+for p in range(chaaa):
+      for g in range(p):
+           if(choa[g]<choa[p]):
+                chea+=choa[g]
+print(chea)
