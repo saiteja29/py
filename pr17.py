d38440ada1cd43a352b34830bd2d257711c1ee7a
saiteja29 <44159659+saiteja29@users.noreply.github.com>
2019-08-03 16:44:37 +0530

    Create pr17.py

---
+++ pr17.py
@@ -0,0 +1,9 @@
+from itertools import permutations
+n, k = map(int, input().split())
+x = list(map(int, input().split()))
+for i in permutations(x, 2):
+    if sum(i) == k:
+        print('yes')
+        break
+else:
+    print('no')
