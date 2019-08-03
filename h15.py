4ec9b1708e724b30dfb93d16a0a2a3570c773f95
saiteja29 <44159659+saiteja29@users.noreply.github.com>
2019-08-03 17:00:55 +0530

    Create h15.py

---
+++ h15.py
@@ -0,0 +1,12 @@
+#aaa
+n=int(input())
+l=list(map(int, input().split()))[:n]
+mx=max(l)
+fnl=[]
+for i in range(0,len(l)):
+    new=l[i:]
+    fi=max(new)
+    if(l[i]==fi):
+        fnl.append(l[i])
+print(*fnl)
+print(mx)
